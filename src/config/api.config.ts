d53923c39c756c498e18941ef57f4d9de4d41032
Felipe Bezerra <felipe.bezerra@live.com>
2019-10-24 18:49:53 +0100

    Listando Categorias com imagens

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://modulo-spring-ionic-felipe.s3.eu-west-2.amazonaws.com"
 }
