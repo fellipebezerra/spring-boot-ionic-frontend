e58f24df8cfc281d7786cc72934e5ab6a4bb0cca
Felipe Bezerra <felipe.bezerra@live.com>
2019-10-29 14:54:05 +0000

    Interceptor para incluir token nas requisições

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -11,7 +11,6 @@ export class ErrorInterceptor implements HttpInterceptor {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("passou no interceptor");
         return next.handle(req)
         .catch((error, caught) => {
 
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -9,16 +9,10 @@ import { StorageService } from "../storge.service";
 export class ClienteService {
     constructor(public http: HttpClient, public storage: StorageService) {}
 
-    findByEmail(email: string) : Observable<ClienteDTO> {
-
-        let token = this.storage.getLocalUser().token;
-        let authHeader = new HttpHeaders({'Authorization': 'Bearer ' + token})
-
-        return this.http.get<ClienteDTO>(
-            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`
-            ,{'headers': authHeader});
+    findByEmail(email: string) : Observable<ClienteDTO> {        
+        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
-    
+
     getImageFromBucket(id : string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
         return this.http.get(url, {responseType : 'blob'});
