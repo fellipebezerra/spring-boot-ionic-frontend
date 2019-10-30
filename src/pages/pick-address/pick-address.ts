973a2e3260b83a36f5a9de4f3d46e541ed3e4c81
Felipe Bezerra <felipe.bezerra@live.com>
2019-10-30 16:21:08 +0000

    buscando endereço da base de dados

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { CartService } from '../../services/domain/cart.service';
+import { StorageService } from '../../services/storge.service';
 
 @IonicPage()
 @Component({
@@ -11,27 +14,25 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-      id:"1",
-      logradouro:"Rua teste",
-      numero:"13",
-      complemento:null,
-      bairro:"Centro",
-      cep:"12345678",
-      cidade:{
-        id:"3",
-        nome:"São Paulo",
-        estado:{
-          id:"3",
-          nome:"São Paulo"
-        }
-      }
-    }];
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+        },
+        error => {});
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
+    }
   }
-
 }
