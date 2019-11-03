f8e09645db3ea973446c42fbbbabb73d7c6dc0f0
Felipe Bezerra <felipe.bezerra@live.com>
2019-11-03 21:42:22 +0000

    refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -21,6 +21,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+      this.loadData();
+  }
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -55,6 +59,15 @@ export class ProdutosPage {
     });
     loader.present();
     return loader;
+  }
+
+  doRefresh(refresher) {
+    //this.page = 0;
+    //this.items = [];
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
   }
 
 }
