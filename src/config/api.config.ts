25ad93554b497c24f4693db13f407234941f1c3c
fellipebezerra <felipe.bezerra@live.com>
2019-11-05 17:31:39 +0000

    teste

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://modelo-spring-ionic-felipe.herokuapp.com",
     bucketBaseUrl: "https://modulo-spring-ionic-felipe.s3.eu-west-2.amazonaws.com"
 }
